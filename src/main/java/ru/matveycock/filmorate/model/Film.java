acb586d7d3f05f35f0f81774b2cf84977d16835f
matveycock <chaccki14@gmail.com>
2023-10-20 15:04:07 +0300

    create models

---
+++ src/main/java/ru/matveycock/filmorate/model/Film.java
@@ -0,0 +1,17 @@
+package ru.matveycock.filmorate.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+public class Film {
+
+    private Integer  id;
+    private String name;
+    private String description;
+    private LocalDate releaseDate;
+    private Integer duration;
+}
---
+++ src/main/java/ru/matveycock/filmorate/model/User.java
@@ -0,0 +1,18 @@
+package ru.matveycock.filmorate.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+public class User {
+
+    private Integer  id;
+    private String email;
+    private String login;
+    private String name;
+    private LocalDate birthday;
+
+}
