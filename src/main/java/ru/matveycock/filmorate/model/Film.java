77b985dda3235e176111235f17f12389a32f6e8f
matveycock <chaccki14@gmail.com>
2023-10-23 20:03:46 +0300

    fix models

--- src/main/java/ru/matveycock/filmorate/model/Film.java
+++ src/main/java/ru/matveycock/filmorate/model/Film.java
@@ -1,5 +1,10 @@
 package ru.matveycock.filmorate.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,9 +14,19 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class Film {
 
-    private Integer  id;
+    private Integer id;
+
+    @NotNull
+    @NotBlank(message = "Название фильма пустое")
     private String name;
+
+    @Size(max = 200, message = "Описание не более 200 символов")
     private String description;
+
+    @JsonFormat(pattern="yyyy-MM-dd")
     private LocalDate releaseDate;
+
+    @NotNull
+    @Positive(message = "Продолжительность фильма отрицательна")
     private Integer duration;
 }
--- src/main/java/ru/matveycock/filmorate/model/User.java
+++ src/main/java/ru/matveycock/filmorate/model/User.java
@@ -1,5 +1,10 @@
 package ru.matveycock.filmorate.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,10 +14,21 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class User {
 
-    private Integer  id;
+    private Integer id;
+
+    @Email(message = "Электронная почта невалидна")
+    @NotNull
+    @NotBlank(message = "Адрес почты пустой")
     private String email;
+
+    @NotNull
+    @NotBlank(message = "Логин пустой")
     private String login;
+
     private String name;
+
+    @JsonFormat(pattern="yyyy-MM-dd")
+    @Past(message = "День рождения должен быть в прошлом")
     private LocalDate birthday;
 
 }
