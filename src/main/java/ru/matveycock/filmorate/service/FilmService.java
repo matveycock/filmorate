d6aa2e67a263f89d0f4b2a210b9a824072579334
matveycock <chaccki14@gmail.com>
2023-11-06 00:07:07 +0300

    add : add User and Film controllers

--- src/main/java/ru/matveycock/filmorate/service/FilmService.java
+++ src/main/java/ru/matveycock/filmorate/service/FilmService.java
@@ -1,4 +1,26 @@
 package ru.matveycock.filmorate.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import ru.matveycock.filmorate.storage.FilmStorage;
+
+@Service
+@Slf4j
 public class FilmService {
+    private final FilmStorage filmStorage;
+    private final UserService userService;
+
+    @Autowired
+    public FilmService(@Qualifier("FilmDbStorage") FilmStorage filmStorage, UserService userService) {
+        this.filmStorage = filmStorage;
+        this.userService = userService;
+    }
+
+
+
+    //updateFilm
+    //addFilm
+    //getAllFilms
 }
--- src/main/java/ru/matveycock/filmorate/service/UserService.java
+++ src/main/java/ru/matveycock/filmorate/service/UserService.java
@@ -1,4 +1,23 @@
 package ru.matveycock.filmorate.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import ru.matveycock.filmorate.storage.FilmStorage;
+import ru.matveycock.filmorate.storage.UserStorage;
+
+@Service
+@Slf4j
 public class UserService {
+    private final UserStorage userStorage;
+
+    @Autowired
+    public UserService(@Qualifier("UserDbStorage") UserStorage userStorage) {
+        this.userStorage = userStorage;
+    }
+
+    //updateUser
+    //addUser
+    //getAllUsers
 }
