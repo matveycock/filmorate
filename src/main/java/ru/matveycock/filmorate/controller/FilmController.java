1acd1efd31dbd0d4692d4e49858f5cf47badbb66
matveycock <chaccki14@gmail.com>
2023-11-06 00:07:38 +0300

    upd : update controllers for service classes

--- src/main/java/ru/matveycock/filmorate/controller/FilmController.java
+++ src/main/java/ru/matveycock/filmorate/controller/FilmController.java
@@ -2,10 +2,14 @@ package ru.matveycock.filmorate.controller;
 
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.matveycock.filmorate.exception.ValidationException;
 import ru.matveycock.filmorate.model.Film;
+import ru.matveycock.filmorate.service.FilmService;
+import ru.matveycock.filmorate.storage.FilmStorage;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -19,12 +23,20 @@ import static ru.matveycock.filmorate.service.ValidationService.validateFilm;
 @Validated
 public class FilmController {
 
+    private final FilmService filmService;
+    private final FilmStorage filmStorage;
     private final Map<Integer, Film> films = new HashMap();
-    private int count = 0;
+
+    @Autowired
+    public FilmController(@Qualifier("FilmDbStorage") FilmStorage filmStorage, FilmService filmService) {
+        this.filmStorage = filmStorage;
+        this.filmService = filmService;
+    }
 
 
     @PutMapping("/films")
     public void updateFilm(@Valid @RequestBody Film film){
+        //filmService.updateFilm(film);
         try{
             validateFilm(film);
             films.remove(film.getId());
@@ -38,6 +50,7 @@ public class FilmController {
 
     @PostMapping("/films")
     public void addFilm(@Valid @RequestBody Film film){
+        //filmService.addFilm(film);
         try{
             if(films.containsKey(film.getId())){
                 log.error("Полученный фильм уже существует");
@@ -53,6 +66,7 @@ public class FilmController {
 
     @GetMapping("/films")
     public List<Film> getAllFilms(){
+        //filmService.getAllFilm(film);
         log.info("Был выполнен запрос всех фильмов");
         return new ArrayList<Film>(films.values());
     }
--- src/main/java/ru/matveycock/filmorate/controller/UserController.java
+++ src/main/java/ru/matveycock/filmorate/controller/UserController.java
@@ -2,10 +2,16 @@ package ru.matveycock.filmorate.controller;
 
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.matveycock.filmorate.exception.ValidationException;
 import ru.matveycock.filmorate.model.User;
+import ru.matveycock.filmorate.service.FilmService;
+import ru.matveycock.filmorate.service.UserService;
+import ru.matveycock.filmorate.storage.FilmStorage;
+import ru.matveycock.filmorate.storage.UserStorage;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,10 +24,21 @@ import static ru.matveycock.filmorate.service.ValidationService.validateUser;
 @RestController
 @Validated
 public class UserController {
+
+    private final UserService userService;
+    private final UserStorage userStorage;
     private final Map<Integer, User> users = new HashMap();
+
+    @Autowired
+    public UserController(@Qualifier("UserDbStorage") UserStorage userStorage, UserService userService) {
+        this.userStorage = userStorage;
+        this.userService = userService;
+    }
+
 
     @PutMapping("/users")
     public void updateUser(@Valid @RequestBody User user){
+        //userService.updateUser(user);
         try {
             validateUser(user);
             users.remove(user.getId());
@@ -33,6 +50,7 @@ public class UserController {
 
     @PostMapping("/users")
     public void addUser(@Valid @RequestBody User user){
+        //userService.addUser(user);
         try {
             validateUser(user);
             users.put(user.getId(), user);
@@ -43,6 +61,7 @@ public class UserController {
 
     @GetMapping("/users")
     public List<User> getAllUsers(){
+        //userService.getAllUser(user);
         log.info("Был выполнен запрос всех пользователей");
         return  new ArrayList<User>(users.values());
     }
